4461246bea68a490eb2ead331c4d3c6303195be5
yoha <yohaseelan@outlook.com>
2026-07-13 08:53:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ app.use(express.json());
 import studentRoutes from "./routes/studentRoutes.js";
 
 app.use("/api/students", studentRoutes);
-
+app.get("/", (req, res) => {
+    res.send("Welcome to the Student API");
+});
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
